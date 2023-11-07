9031d106b6aaae5fcfe1a2cd4d0431aa6f3b0403
Matteo Prandi <matteo.prandi@live.it>
2023-11-07 18:49:53 +0100

    init: main.py first restyle, introduced lifespan management

--- app/main.py
+++ app/main.py
@@ -1,12 +1,46 @@
+import logging
+
 import uvicorn
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from app.config.config import settings
+from app.api.router import api_router
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+logger = logging.getLogger(__name__)
+
+def __setup_logging(log_level: str):
+    log_level = getattr(logging, log_level.upper())
+    log_formatter = logging.Formatter(
+        "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"
+    )
+    root_logger = logging.getLogger()
+    root_logger.setLevel(log_level)
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(log_formatter)
+    root_logger.addHandler(stream_handler)
+    logger.info("Set up logging with log level %s", log_level)
+    
+    
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+   #start models
+   yield
+   #shutdown models 
+
+app = FastAPI(title=settings.APP_NAME, version=settings.VERSION, lifespan=lifespan)
+app.include_router(api_router, prefix=settings.API_PREFIX)
 
 def start():
+    print("Running in AppEnvironment: " + settings.ENVIRONMENT.value)
+    __setup_logging(settings.LOG_LEVEL)
+    
     """Launched with `poetry run start` at root level"""
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        workers=settings.UVICORN_WORKER_COUNT
+    )
