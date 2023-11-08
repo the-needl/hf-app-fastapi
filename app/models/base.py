27fb29e8599e2d3b5efa4389b17a600c305840a8
Matteo Prandi <matteo.prandi@live.it>
2023-11-08 01:00:40 +0100

    fix: introduced load_local_model in BaseModel

--- app/models/base.py
+++ app/models/base.py
@@ -1,25 +1,37 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
+
+from pathlib import Path
 
 import logging
 from abc import abstractmethod
 
-from transformers import AutoTokenizer
-from transformers import AutoModelForQuestionAnswering
-from transformers import pipeline
-
-from app.services.utils import ModelLoader
-from app.config.config import settings
+import torch
+from transformers import PreTrainedModel
+from transformers import PreTrainedTokenizer
 
 logger = logging.getLogger(__name__)
 
 class BaseModel:
-    def __init__(self, path=settings.DEFAULT_MODEL_PATH):
-        self.path = path
-        self._load_local_model()
-
-    @abstractmethod
+    def __init__(self,
+                 model_name: str,
+                 model_directory: str,
+                 tokenizer_loader: PreTrainedTokenizer,
+                 model_loader: PreTrainedModel):
+        
+        self.model_name = Path(model_name)
+        self.model_directory = Path(model_directory)
+        self.tokenizer_loader = tokenizer_loader
+        self.model_loader = model_loader
+        
     def _load_local_model(self):
-        pass
+        self.save_path = self.model_directory / self.model_name
+        
+        if not self.save_path.exists():
+            logger.debug(f"[+] {self.save_path} does not exit!")
+            self.save_path.mkdir(parents=True, exist_ok=True)
+            self.__download_model()
+
+        self.tokenizer, self.model = self.__load_model()
 
     @abstractmethod
     def _pre_process(self):
@@ -39,3 +51,41 @@ class BaseModel:
     @abstractmethod
     def output(self):
         pass
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}(model={self.save_path})"
+
+    # Download model from HuggingFace
+    def __download_model(self) -> None:
+
+        logger.debug(f"[+] Downloading {self.model_name}")
+        tokenizer = self.tokenizer_loader.from_pretrained(f"{self.model_name}")
+        model = self.model_loader.from_pretrained(f"{self.model_name}")
+
+        logger.debug(f"[+] Saving {self.model_name} to {self.save_path}")
+        tokenizer.save_pretrained(f"{self.save_path}")
+        model.save_pretrained(f"{self.save_path}")
+
+        logger.debug("[+] Process completed")
+
+    # Load model
+    def __load_model(self) -> Tuple:
+
+        logger.debug(f"[+] Loading model from {self.save_path}")
+        tokenizer = self.tokenizer_loader.from_pretrained(f"{self.save_path}")
+        # Check if GPU is available
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        logger.info(f"[+] Model loaded in {device} complete")
+        model = self.model_loader.from_pretrained(f"{self.save_path}").to(device)
+
+        logger.debug("[+] Loading completed")
+        return tokenizer, model
+
+    def retrieve(self) -> Tuple:
+
+        """Retriver
+
+        Returns:
+            Tuple: tokenizer, model
+        """
+        return self.tokenizer, self.model
