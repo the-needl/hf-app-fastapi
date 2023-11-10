1c7f9d699e81f6e0d9fbbe757d6b6314c0d1d153
Matteo Prandi <matteo.prandi@live.it>
2023-11-10 22:03:23 +0100

    temp: removed health check reference

--- app/api/router.py
+++ app/api/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import health, SUM#, NER, KEY, QA
+from app.api.endpoints import SUM#, health, NER, KEY, QA
 
 api_router = APIRouter()
 
 api_router.include_router(SUM.router, prefix="/sum", tags=["summary"])
-api_router.include_router(health.router, prefix="/alive", tags=["health"])
+# api_router.include_router(health.router, prefix="/alive", tags=["health"])
