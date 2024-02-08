07f8b26af41d2a9397178e76526dc14974ac2cee
Matteo Prandi <matteo.prandi@live.it>
2024-02-08 22:10:40 +0100

    init: added support for Embeddings models with new alternative Base class

--- app/engine/models.py
+++ app/engine/models.py
@@ -8,6 +8,7 @@ import importlib
 
 import torch
 from transformers import PreTrainedModel, PreTrainedTokenizer
+from sentence_transformers import SentenceTransformer
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import tiktoken
@@ -133,3 +134,115 @@ def create_instance(model_type: str) -> Base:
         print(f"Class {model_type}Model not found.")
         
     return model_class()
+
+
+class EmbeddingBase:
+    def __init__(self,
+                 model_name: str,
+                 model_path: str,
+                 model_loader: SentenceTransformer,
+                 ):
+        
+        self.model_name = Path(model_name)
+        self.model_path = Path(model_path)
+        self.model_loader = model_loader
+        
+        self.save_path = self.model_path / self.model_name
+        
+        if not self.save_path.exists():
+            logger.debug(f"[+] {self.save_path} does not exit!")
+            self.save_path.mkdir(parents=True, exist_ok=True)
+            self.__download_model()
+        else:
+            logger.debug(f"[+] Model already existing, loading from {self.save_path}")
+
+        self.tokenizer, self.model = self.__load_model()
+
+    @abstractmethod
+    def _pre_process(self):
+        pass
+    
+    @abstractmethod
+    def _post_process(self):
+        pass
+    
+    @abstractmethod
+    def _output(self):
+        pass
+
+    @abstractmethod
+    def output(self):
+        pass
+    
+    def _get_context_len(self, context: str) -> int:
+        """
+        Return context token length.
+        """
+        context_len = len(self.tokenizer.encode(context))
+        # logger.debug(f"[-] Context length: {context_len}")
+        print(f"[-] Context length: {context_len}")
+        
+        return context_len
+    
+    def _split_context(self,
+                       context: str,
+                       chunk_length: int = None) -> List:
+        """
+        Return context as list of chunks only if len(context) <= len(chunk).
+        """
+        if not chunk_length:
+            chunk_length = self.tokenizer.model_max_length
+            
+        # logger.debug(f"[-] Max model token length: {chunk_length}")
+        print(f"[-] Max model token length: {chunk_length}")
+        
+        if self._get_context_len(context) <= chunk_length:
+            texts = [context]
+        else:
+            text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=int(chunk_length*0.9), chunk_overlap=0
+                )
+            texts = text_splitter.split_text(context)
+            
+        return texts
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}(model={self.save_path})"
+
+    # Download model from HuggingFace
+    def __download_model(self) -> None:
+        logger.debug(f"[+] Downloading {self.model_name}")
+        model = self.model_loader(f"{self.model_name}")
+
+        logger.debug(f"[+] Saving {self.model_name} to {self.save_path}")
+        model.save(f"{self.save_path}")
+
+        logger.debug("[+] Process completed")
+
+    # Load model
+    def __load_model(self) -> Tuple:
+        logger.debug(f"[+] Loading model from {self.save_path}")
+        model = self.model_loader(f"{self.save_path}")
+
+        logger.debug("[+] Loading completed")
+        return model
+
+    def retrieve(self) -> Tuple:
+        """Retriver
+
+        Returns:
+            Tuple: tokenizer, model
+        """
+        return self.tokenizer, self.model
+    
+    
+def create_instance(model_type: str) -> Base:
+    try:
+        module = importlib.import_module(f"app.engine.instances.{model_type}")
+        model_class = getattr(module, f"{model_type}Model")
+    except ImportError:
+        print(f"Module {model_type} not found.")
+    except AttributeError:
+        print(f"Class {model_type}Model not found.")
+        
+    return model_class(SentenceTransformer)
